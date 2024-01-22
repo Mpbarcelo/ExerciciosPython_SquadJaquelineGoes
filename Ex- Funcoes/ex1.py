ecfa3ef7dd12bac25b3c72bdbf892eb8fb2cff40
Mpbarcelo <marciatrevinho@gmail.com>
2024-01-22 09:20:30 -0300

    primeiro exercicio novamente

--- Ex- Funcoes/ex1.py
+++
@@ -1,7 +0,0 @@
-# exercicio 02
-def inverter_numero(numero):
-    return int(str(numero)[::-1])
-
-numero = int(input("Digite um número inteiro: "))
-resultado = inverter_numero(numero)
-print(f"Número invertido: {resultado}")
