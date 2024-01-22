c23a024ccdb3ee0a93138016a4efcc1061c9122e
Mpbarcelo <marciatrevinho@gmail.com>
2024-01-22 09:18:06 -0300

    terceiro exercicio novamente

--- Ex- Funcoes/ex1.py
+++ Ex- Funcoes/ex1.py
@@ -1,12 +1,7 @@
-# exercicio 01
-def somar_numeros(a, b, c):
-    return a + b + c
+# exercicio 02
+def inverter_numero(numero):
+    return int(str(numero)[::-1])
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-num3 = float(input("Digite o terceiro número: "))
-
-resultado = somar_numeros(num1, num2, num3)
-print(f"A soma dos três números é: {resultado}")
-
-
+numero = int(input("Digite um número inteiro: "))
+resultado = inverter_numero(numero)
+print(f"Número invertido: {resultado}")
